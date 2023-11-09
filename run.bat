c535a1dd656d05ca7c7ad9d9a53243e1fb68fe3e
Byan Xia <616613288@qq.com>
2023-11-10 07:46:15 +0800

    New Version

--- run.bat
+++ run.bat
@@ -6,10 +6,5 @@ for %%i in (5 10 15 20 25 30 0.1) do (
 
 for %%i in (5 10 15 20 25 30 0.1) do (
       echo "runing"
-      python demo.py --data="Honghu" --train_num=%%i --epoch=3000 --seed=114
-)
-
-for %%i in (5 10 15 20 25 30 0.1) do (
-      echo "runing"
       python demo.py --data="Pavia" --train_num=%%i --epoch=3000 --seed=114
 )
